8f4341fc7a89e4cf76658d4675d58e516c66a1b4
Andrea Josefina Montoya Ruiz <andrea.montoya@ed.company>
2021-06-27 14:46:28 -0300

    feat: modificando metodos do onClick

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,7 @@
+/* eslint-disable react/no-direct-mutation-state */
 import React from 'react';
 import Btn from '../../components/btn';
-//import { useState, useEffect } from "react";
-//import NumberFormat from "react-number-format";
+import Input from "../../components/Input";
 
 class Main extends React.Component {
     constructor(props){
@@ -12,141 +12,124 @@ class Main extends React.Component {
             ['AC' , 'btn light-gray', 'reset' ],
             ['+/-', 'btn light-gray', 'minusPlus' ],
             ['%', 'btn light-gray', 'percent' ],
-            ['÷', 'btn operador', 'operatorType'],
+            ['÷', 'btn operador', 'divide'],
             ['7', 'btn', 'inputNum'],
             ['8', 'btn', 'inputNum'],
             ['9', 'btn', 'inputNum'],
-            ['x', 'btn operador', 'operatorType'],
+            ['x', 'btn operador', 'divide'],
             ['4', 'btn', 'inputNum'],
             ['5', 'btn', 'inputNum'],
             ['6', 'btn', 'inputNum'],
-            ['-', 'btn operador', 'operatorType'],
+            ['-', 'btn operador', 'subtract'],
             ['1', 'btn', 'inputNum'],
             ['2', 'btn', 'inputNum'],
             ['3', 'btn', 'inputNum'],
-            ['+', 'btn operador', 'operatorType'],
-            ['0', 'btn zero', 'inputNum'],
+            ['+', 'btn operador', 'add'],
+            ['0', 'btn zero', 'addZeroToInput'],
             ['.', 'btn', 'inputNum'],
             ['=', 'btn purple', 'equals'],
       ],
-      preState: "",
-      curState: "",
-      input: "0",
-      operator: null,
-      total: false
+      input: "",
+      previousNumber: "",
+      currentNumber: "",
+      operator: ""
       }
     }
     }
-      
-      /*const [preState, setPreState] = useState("");
-      const [curState, setCurState] = useState("");
-      const [input, setInput] = useState("0");
-      const [operator, setOperator] = useState(null);
-      const [total, setTotal] = useState(false);  */
 
-     /*  const inputNum = (e) => {
-        if (this.state.data.curState.includes(".") && e.target.innerText === ".") return;
+    inputNum = val => {
+      this.setState({ input: this.state.input + val });
+    };
+
+    addDecimal = val => {
+      // only add decimal if there is no current decimal point present in the input area
+      if (this.state.input.indexOf(".") === -1) {
+        this.setState({ input: this.state.input + val });
+      }
+    };
+  
+    addZeroToInput = val => {
+      // if this.state.input is not empty then add zero
+      if (this.state.input !== "") {
+        this.setState({ input: this.state.input + val });
+      }
+    };
+  
+    reset = () => {
+      this.setState({ input: "" });
+    };
+  
+    add = () => {
+      this.state.previousNumber = this.state.input;
+      this.setState({ input: "" });
+      this.state.operator = "plus";
+    };
+  
+    subtract = () => {
+      this.state.previousNumber = this.state.input;
+      this.setState({ input: "" });
+      this.state.operator = "subtract";
+    };
     
-        if (this.state.data.total) {
-          setPreState("");
-        }
-    
-        this.state.data.curState
-          ? setCurState((pre) => pre + e.target.innerText)
-          : setCurState(e.target.innerText);
-        setTotal(false);
-      };
-    
-      useEffect(() => {
-        setInput(this.state.data.curState);
-      }, [this.state.data.curState]);
-    
-      useEffect(() => {
-        setInput("0");
-      }, []);
-      const operatorType = (e) => {
-        setTotal(false);
-        setOperator(e.target.innerText);
-        if (this.state.data.curState === "") return;
-        if (this.state.data.preState !== "") {
-          equals();
-        } else {
-          setPreState(this.state.data.curState);
-          setCurState("");
-        }
-      };
-    
-      const equals = (e) => {
-        if (e?.target.innerText === "=") {
-          setTotal(true);
-        }
-        let cal;
-        switch (this.state.data.operator) {
-          case "÷":
-            cal = String(parseFloat(this.state.data.preState) / parseFloat(this.state.data.curState));
-            break;
-    
-          case "+":
-            cal = String(parseFloat(this.state.data.preState) + parseFloat(this.state.data.curState));
-            break;
-          case "x":
-            cal = String(parseFloat(this.state.data.preState) * parseFloat(this.state.data.curState));
-            break;
-          case "-":
-            cal = String(parseFloat(this.state.data.preState) - parseFloat(this.state.data.curState));
-            break;
-          default:
-            return;
-        }
-        setInput("");
-        setPreState(cal);
-        setCurState("");
-      };
-    
-      const minusPlus = () => {
-        if (this.state.data.curState.charAt(0) === "-") {
-          setCurState(this.state.data.curState.substring(1));
-        } else {
-          setCurState("-" + this.state.data.curState);
-        }
-      };
-    
-      const percent = () => {
-        this.state.data.preState
-          ? setCurState(String((parseFloat(this.state.data.curState) / 100) * this.state.data.preState))
-          : setCurState(String(parseFloat(this.state.data.curState) / 100));
-      };
-    
-      const reset = () => {
-        setPreState("");
-        setCurState("");
-        setInput("0");
-      }; */
-      render(){
-        return (
-          <div className='container'>
-            <div className='wrapper'>
-              <div className='screen'>
-                {this.state.data.input !== "" || this.state.data.input === "0" ? (
-                  <NumberFormat
-                    value={this.state.data.input}
-                    displayType={"text"}
-                    thousandSeparator={true}
-                  />
-                ) : (
-                  <NumberFormat
-                    value={this.state.data.preState}
-                    displayType={"text"}
-                    thousandSeparator={true}
-                  />
-                )}
-              </div>
+    multiply = () => {
+      this.state.previousNumber = this.state.input;
+      this.setState({ input: "" });
+      this.state.operator = "multiply";
+    };
+  
+    divide = () => {
+      this.state.previousNumber = this.state.input;
+      this.setState({ input: "" });
+      this.state.operator = "divide";
+    };
+  
+    equals= () => {
+      this.state.currentNumber = this.state.input;
+  
+      if (this.state.operator === "plus") {
+        this.setState({
+          input:
+            parseInt(this.state.previousNumber) +
+            parseInt(this.state.currentNumber)
+        });
+      } else if (this.state.operator === "subtract") {
+        this.setState({
+          input:
+            parseInt(this.state.previousNumber) -
+            parseInt(this.state.currentNumber)
+        });
+      } else if (this.state.operator === "multiply") {
+        this.setState({
+          input:
+            parseInt(this.state.previousNumber) *
+            parseInt(this.state.currentNumber)
+        });
+      } else if (this.state.operator === "divide") {
+        this.setState({
+          input:
+            parseInt(this.state.previousNumber) /
+            parseInt(this.state.currentNumber)
+        });
+      }
+    };
+    render() {
+      return (
+        <div className="container">
+          <div className="wrapper">
+            <div className="screen">
+              <Input>{this.state.input}</Input>
+            </div>     
               <Btn cont={this.state.data.buttom} ></Btn>
             </div>
           </div>
-        );
-      }
-    
-}
 
+      );
+    }
+  }
+    
+    
+       
 export default Main;
+
+
+ 
